fe7fd1aef633c001832290c3f89a3b50e9664275
Eric Lynch <lynch.er18@gmail.com>
2018-07-14 00:35:25 -0400

    Camera scripting

---
+++ Assets/Scripts/Camera/CameraBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraBase : MonoBehaviour {
+
+    [SerializeField] float m_Timer = 0;
+
+
+    void Start () {
+		
+	}
+
+
+    public void SetTimer(float time)
+    {
+
+        m_Timer = time;
+
+    }
+
+    public bool IsTimedOut()
+    {
+
+        return m_Timer <= 0;
+
+    }
+	
+
+	void Update () {
+
+        m_Timer -= Time.deltaTime;
+
+	}
+
+    
+}
